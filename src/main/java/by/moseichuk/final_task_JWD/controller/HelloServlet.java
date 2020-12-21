09360d22d76c3fdbca88de22ac23885037d99c54
Alexandr <digger_sasha@mail.ru>
2020-12-21 18:17:20 +0300

    add init method

--- src/main/java/by/moseichuk/final_task_JWD/controller/HelloServlet.java
+++ src/main/java/by/moseichuk/final_task_JWD/controller/HelloServlet.java
@@ -1,25 +1,31 @@
 package by.moseichuk.final_task_JWD.controller;
 
+import by.moseichuk.final_task_JWD.dao.exception.ConnectionPoolException;
+import by.moseichuk.final_task_JWD.dao.pool.ConnectionPool;
+
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
     public void init() {
-        message = "Hello World!";
+        try {
+            ConnectionPool.getInstance().init("org.mariadb.jdbc.Driver",
+                    "jdbc:mariadb://localhost:3306/adlinker_db", "root", "root",
+                    3, 5, 2);
+        } catch (ConnectionPoolException e) {
+            destroy();
+        }
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+       //TODO
+    }
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //TODO
     }
 
     public void destroy() {
