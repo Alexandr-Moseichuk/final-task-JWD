82d97505c41d1e000abc51f31db975b9b130ad70
Alexandr <digger_sasha@mail.ru>
2020-12-24 18:05:20 +0300

    add logger

--- src/main/java/by/moseichuk/final_task_JWD/controller/CommandFilter.java
+++ src/main/java/by/moseichuk/final_task_JWD/controller/CommandFilter.java
@@ -6,6 +6,8 @@ import by.moseichuk.final_task_JWD.dao.exception.TransactionException;
 import by.moseichuk.final_task_JWD.dao.transaction.TransactionFactoryImpl;
 import by.moseichuk.final_task_JWD.service.UserService;
 import by.moseichuk.final_task_JWD.service.impl.UserServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -14,10 +16,11 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CommandFilter implements Filter {
-    private static Map<String, Class<? extends Command>> commandMap = new ConcurrentHashMap<>();
+    private static final Logger LOGGER = LogManager.getLogger(CommandFilter.class);
+    private static Map<String, Command> commandMap = new ConcurrentHashMap<>();
 
     static {
-        commandMap.put("/login", Login.class);
+        commandMap.put("/login", new Login());
     }
 
     @Override
@@ -29,24 +32,24 @@ public class CommandFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         if (servletRequest instanceof HttpServletRequest) {
             HttpServletRequest request = (HttpServletRequest) servletRequest;
-            String method = request.getMethod();
 
             int begin = request.getContextPath().length();
             String commandName = request.getRequestURI().substring(begin);
-            Class<? extends Command> commandClass = commandMap.get(commandName);
-            try {
-                Command command = commandClass.newInstance();
+            Command command = commandMap.get(commandName);
+
+            if (command != null) {
                 command.setName(commandName);
                 request.setAttribute("command", command);
                 filterChain.doFilter(servletRequest, servletResponse);
-            } catch (InstantiationException | IllegalAccessException e) {
-                //TODO logger
-                request.setAttribute("errorMessage", String.format("Ошибка сервера по адресу %s, описание ошибки %s", request.getRequestURI(), e.getMessage()));
+            } else {
+                request.setAttribute("errorMessage", String.format("Ошибка сервера по адресу %s, нет комманды", request.getRequestURI()));
                 request.getRequestDispatcher("/WEB-INF/jsp/error.jsp").forward(servletRequest, servletResponse);
             }
-
         } else {
-            //TODO logger(not HTTP request)
+            LOGGER.error(String.format("Impossible to perform request (Not HTTP request). From : %s. Host : %s. Port : %s",
+                                       servletRequest.getRemoteAddr(),
+                                       servletRequest.getRemoteHost(),
+                                       servletRequest.getRemotePort()));
         }
     }
 
