eb44a5c3d9f33d2124818a88748e6ed1392e1eac
Alexandr <digger_sasha@mail.ru>
2021-01-16 12:25:35 +0300

    add redirect when user authorized

--- src/main/java/by/moseichuk/final_task_JWD/controller/command/show/IndexVisual.java
+++ src/main/java/by/moseichuk/final_task_JWD/controller/command/show/IndexVisual.java
@@ -1,11 +1,13 @@
 package by.moseichuk.final_task_JWD.controller.command.show;
 
+import by.moseichuk.final_task_JWD.bean.User;
 import by.moseichuk.final_task_JWD.bean.UserRole;
 import by.moseichuk.final_task_JWD.controller.Command;
 import by.moseichuk.final_task_JWD.controller.Forward;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 
 public class IndexVisual extends Command {
@@ -16,6 +18,17 @@ public class IndexVisual extends Command {
 
     @Override
     public Forward execute(HttpServletRequest request, HttpServletResponse response) {
-        return new Forward("jsp/index.jsp");
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return new Forward("jsp/index.jsp");
+        } else {
+            User authorizedUser = (User) session.getAttribute("authorizedUser");
+            if (authorizedUser == null) {
+                return new Forward("jsp/index.jsp");
+            } else {
+                return new Forward("/campaign/list", true);
+            }
+
+        }
     }
 }
--- src/main/java/by/moseichuk/final_task_JWD/controller/command/show/LoginVisual.java
+++ src/main/java/by/moseichuk/final_task_JWD/controller/command/show/LoginVisual.java
@@ -1,14 +1,26 @@
 package by.moseichuk.final_task_JWD.controller.command.show;
 
+import by.moseichuk.final_task_JWD.bean.User;
 import by.moseichuk.final_task_JWD.controller.Command;
 import by.moseichuk.final_task_JWD.controller.Forward;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginVisual extends Command {
     @Override
     public Forward execute(HttpServletRequest request, HttpServletResponse response) {
-        return new Forward("jsp/login.jsp");
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return new Forward("jsp/login.jsp");
+        } else {
+            User authorizedUser = (User) session.getAttribute("authorizedUser");
+            if (authorizedUser == null) {
+                return new Forward("jsp/login.jsp");
+            } else {
+                return new Forward("/campaign/list", true);
+            }
+        }
     }
 }
