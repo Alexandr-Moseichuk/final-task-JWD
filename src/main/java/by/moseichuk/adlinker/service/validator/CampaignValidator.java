13c01800d0ed3f43e3262d318acaecd7cd3671c5
Alexandr <digger_sasha@mail.ru>
2021-01-28 12:07:55 +0300

    Update CampaignValidator.java

--- src/main/java/by/moseichuk/adlinker/service/validator/CampaignValidator.java
+++ src/main/java/by/moseichuk/adlinker/service/validator/CampaignValidator.java
@@ -6,15 +6,27 @@ import by.moseichuk.adlinker.service.Validator;
 import java.util.Map;
 
 public class CampaignValidator implements Validator<Campaign> {
+    private static final int MIN_TITLE_LENGTH = 5;
+    private static final int MAX_TITLE_LENGTH = 50;
+    private static final int MIN_DESCRIPTION_LENGTH = 30;
+    private static final int MAX_DESCRIPTION_LENGTH = 3000;
+    private static final int MIN_REQUIREMENT_LENGTH = 30;
+    private static final int MAX_REQUIREMENT_LENGTH = 3000;
+
+
+    //TODO static final
     @Override
     public Map<String, String> validate(Campaign campaign) {
-        if (campaign.getTitle().length() < 5 || campaign.getTitle().length() > 100) {
+        int titleLength = campaign.getTitle().length();
+        if (titleLength < MIN_TITLE_LENGTH || titleLength > MAX_TITLE_LENGTH) {
             errorMap.put("titleError", "Wrong length");
         }
-        if (campaign.getDescription().length() < 30 || campaign.getDescription().length() > 3000) {
+        int descriptionLength = campaign.getDescription().length();
+        if (descriptionLength < MIN_DESCRIPTION_LENGTH || descriptionLength > MAX_DESCRIPTION_LENGTH) {
             errorMap.put("descriptionError", "Wrong length");
         }
-        if (campaign.getRequirement().length() < 30 || campaign.getRequirement().length() > 3000) {
+        int requirementLength = campaign.getRequirement().length();
+        if (requirementLength < MIN_REQUIREMENT_LENGTH || requirementLength > MAX_REQUIREMENT_LENGTH) {
             errorMap.put("requirementError", "Wrong length");
         }
         if (campaign.getBeginDate() == null || campaign.getEndDate() == null) {
