90d46ebd805bb2803dbc92df1296cbda44794199
Alexandr <digger_sasha@mail.ru>
2021-02-01 17:48:54 +0300

    add javadoc

--- src/main/java/by/moseichuk/adlinker/controller/command/Command.java
+++ src/main/java/by/moseichuk/adlinker/controller/command/Command.java
@@ -9,27 +9,68 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Contains actions which user can execute if has access.
+ *
+ * @author Alexandr Moseichuk
+ */
 public abstract class Command {
-    private Set<UserRole> permissionSet = new HashSet<>();
+    /**
+     * Set of user role that can execute command
+     */
+    private final Set<UserRole> permissionSet = new HashSet<>();
+    /**
+     * Factory for creating services implementations
+     */
     protected ServiceFactory serviceFactory;
+    /**
+     * Name of command
+     */
     private String name;
 
+    /**
+     * Returns set of allowed roles
+     *
+     * @return set of allowed roles
+     */
     public Set<UserRole> getPermissionSet() {
         return permissionSet;
     }
 
+    /**
+     * Sets service factory
+     *
+     * @param serviceFactory new service factory
+     */
     public void setServiceFactory(ServiceFactory serviceFactory) {
         this.serviceFactory = serviceFactory;
     }
 
+    /**
+     * Returns command name
+     *
+     * @return command name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets command name
+     *
+     * @param name command name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Executes user command
+     *
+     * @param request  http request
+     * @param response http responce
+     * @return {@code Forward} witch leds to result page
+     */
     public abstract Forward execute(HttpServletRequest request, HttpServletResponse response);
 
     @Override
