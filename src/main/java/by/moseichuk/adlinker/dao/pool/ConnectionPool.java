5d058077e69a651ad003a584780e13464ec6a1da
Alexandr <digger_sasha@mail.ru>
2021-02-02 22:34:27 +0300

    use ReentrantLock

--- src/main/java/by/moseichuk/adlinker/dao/pool/ConnectionPool.java
+++ src/main/java/by/moseichuk/adlinker/dao/pool/ConnectionPool.java
@@ -11,6 +11,9 @@ import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * A thread-safe pool of database connections.
@@ -20,6 +23,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class ConnectionPool {
     private static final Logger LOGGER = LogManager.getLogger(ConnectionPool.class);
     private static final ConnectionPool instance = new ConnectionPool();
+    private static final ReentrantLock lock = new ReentrantLock();
 
     private String url;
     private String username;
@@ -30,12 +34,15 @@ public class ConnectionPool {
     /**
      * Queue of available connections
      */
-    private BlockingQueue<PooledConnection> freeConnections = new LinkedBlockingQueue<>();
+    private final BlockingQueue<PooledConnection> freeConnections = new LinkedBlockingQueue<>();
     /**
      * Set of taken connections
      */
-    private Set<PooledConnection> usedConnections = new ConcurrentSkipListSet<>();
+    private final Set<PooledConnection> usedConnections = new ConcurrentSkipListSet<>();
 
+    /**
+     * Default empty constructor
+     */
     private ConnectionPool() {}
 
 
@@ -49,34 +56,38 @@ public class ConnectionPool {
     }
 
     /**
-     * Returns free connection from the pool and puts connection to {@code userdConnections} list.
+     * Returns free connection from the pool and puts connection to {@code usedConnections} list.
      * If there is no any free connection waits {@code connectionTimeout} seconds
      *
      * @return free connection from the pool.
      * @throws ConnectionPoolException if there is no free connections or can't create new one
      */
-    public synchronized Connection getConnection() throws ConnectionPoolException {
+    public Connection getConnection() throws ConnectionPoolException {
         PooledConnection connection = null;
-       // while(connection == null) {
-            try {
-                if(!freeConnections.isEmpty()) {
-                    connection = freeConnections.take();
-                    if(!connection.isValid(connectionTimeout)) {
-                        try {
-                            connection.getConnection().close();
-                        } catch(SQLException e) {}
-                        connection = null;
+        lock.lock();
+        try {
+            if (!freeConnections.isEmpty()) {
+                connection = freeConnections.take();
+                if(!connection.isValid(connectionTimeout)) {
+                    try {
+                        connection.getConnection().close();
+                    } catch(SQLException e) {
+                        LOGGER.debug("Can't close connection " + e.getMessage());
                     }
-                } else if(usedConnections.size() < poolSize) {
-                    connection = createConnection();
-                } else {
-                    throw new ConnectionPoolException("The limit of number of database connections is exceeded");
+                    connection = null;
                 }
-            } catch(InterruptedException | SQLException e) {
-                throw new ConnectionPoolException("Impossible to connect to a database",e);
+            } else if(usedConnections.size() < poolSize) {
+                connection = createConnection();
+            } else {
+                throw new ConnectionPoolException("The limit of number of database connections is exceeded");
             }
-        //}
-        usedConnections.add(connection);
+            usedConnections.add(connection);
+        } catch(InterruptedException | SQLException e) {
+            throw new ConnectionPoolException("Impossible to connect to a database", e);
+        } finally {
+            lock.unlock();
+        }
+
         LOGGER.debug(String.format("Connection was created. Pool state: %s taken connections; %s free connections", usedConnections.size(), freeConnections.size()));
 
         return connection;
@@ -87,7 +98,7 @@ public class ConnectionPool {
      *
      * @param connection that will be in list of free connections
      */
-    synchronized void freeConnection(PooledConnection connection) {
+    void freeConnection(PooledConnection connection) {
         try {
             if(connection.isValid(connectionTimeout)) {
                 connection.clearWarnings();
@@ -95,11 +106,17 @@ public class ConnectionPool {
                 usedConnections.remove(connection);
                 freeConnections.put(connection);
                 LOGGER.debug(String.format("Connection was returned into pool. Pool state: %s taken connections; %s free connections", usedConnections.size(), freeConnections.size()));
+            } else {
+                usedConnections.remove(connection);
+                connection.getConnection().close();
+                LOGGER.debug("Invalid connection was destroyed while trying to release connection");
             }
         } catch(SQLException | InterruptedException e1) {
             try {
                 connection.getConnection().close();
-            } catch(SQLException e2) {}
+            } catch(SQLException e2) {
+                LOGGER.debug("Can't close connection " + e2.getMessage());
+            }
         }
     }
 
@@ -115,7 +132,7 @@ public class ConnectionPool {
      * @param connectionTimeout number of seconds to timeout connection
      * @throws ConnectionPoolException if can't create connection
      */
-    public synchronized void init(String driverClass, String url, String username, String password,
+    public void init(String driverClass, String url, String username, String password,
                                   int startSize, int poolSize, int connectionTimeout) throws ConnectionPoolException {
         try {
             destroy();
@@ -148,7 +165,7 @@ public class ConnectionPool {
     /**
      * Destroys connection pool and close all connections
      */
-    public synchronized void destroy() {
+    public void destroy() {
         usedConnections.addAll(freeConnections);
         freeConnections.clear();
         for(PooledConnection connection : usedConnections) {
